2c462673cbde450116685088c3bae7967fe6607d
Ersatzhase <102618574+Ersatzhase@users.noreply.github.com>
2022-04-01 11:57:47 +0200

    init

---
+++ CONTROLLER.java
@@ -0,0 +1,87 @@
+/*
+ * Das erste importierte Paket ist wichtig, da der Controller ohne das event-Paket 
+ * nicht wüsste, was ein ActionListener ist. Das zweite Paket braucht man nur, wenn
+ * man Nutzereingaben einlesen möchte (siehe in Methode actionPerformed).
+ */
+
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+ * Die Klasse implementiert das Interface ActionListener, da sie dadurch auf Aktionen
+ * in der View reagieren kann. Auf welche Aktionen wie reagiert wird, wird in der 
+ * Methode actionPerformed festgelegt. 
+ * Tipp: Wenn ihr eine Mausinteraktion haben wollt, könnt ihr dafür die Methode 
+ * mouseClicked(MouseEvent e) verwenden. Ein ActionListener erkennt sie auch 
+ * (da MouseListener von ActionListener erbt).
+ */
+public class CONTROLLER implements ActionListener
+{
+    private VIEW view;
+    private MODEL model;
+
+    /*
+     * Der Controller erstellt ein Model und eine View und übergibt dabei das Model
+     * an die View. Anschließend fügt er die View in die Observer-Liste  des Models ein.
+     * Zuletzt registriert er sich bei den beiden Buttons der View (dort werden 
+     * passende Methoden zur Verfügung gestellt).
+     */
+    public CONTROLLER()
+    {
+        ? = new MODEL();
+        ? = new VIEW(?);
+        //Füge dem model die view als Observer über die entsprechende Methode des models hinzu
+        ?
+        
+        // Man muss die ActionListener anmelden.
+        view.addPlusListener(this);
+        view.addMinusListener(this);
+    }
+
+    /*
+     * In dieser Methode wird festgelegt, wie auf bestimmte Aktionen reagiert werden soll. 
+     * Über die Methode getActionCommand() kann abgefragt werden, welcher Knopf 
+     * gedrückt wurde. Es wird dann die passende Methode im Model aufgerufen.
+     * ACHTUNG: In dieser Variante der Methode ist auch schon das Einlesen von Werten enthalten. 
+     * Wer den ursprünglichen Code ohne diese Funktion haben möchte, musst im ersten if-Teil
+     * nur die Zeile model.inkrementieren() schreiben und die entsprechende Methode
+     * im Model anpassen.
+     * 
+     * Wird in der View ein Button gedrückt, bei dem ein ActionListener hinterlegt ist, so wird diese Methode aufgerufen.
+     * 
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        //Falls der gedrückte Button der + Button ist.
+        if(e.getActionCommand().equals("+"))
+        {
+            try 
+            {
+                /* 
+                 * Es wird ein Eingabefenster (JOptionPane) mit der entsprechenden Nachricht angezeigt.
+                 * Der Inhalt wird auf einen Integer gecastet, falls möglich.
+                 * Ist es nicht möglich, weil der Benutzer z.B. einen Text eingegeben hat,
+                 * kommt der catchBlock zur Aktion: Er greift diesen Fehler ab un zeigt die Fehlermeldung an.
+                 * 
+                 * Funktioniert es, so wird die zahl um den eingegeben Wert erhöht
+                 */
+                int n = Integer.parseInt(JOptionPane.showInputDialog("Welcher Wert soll eingefügt werden?"));
+                
+                //Erhöhe unseren Wert um n. Verwende eine passende Methode im model
+                ?
+            } 
+            catch (NumberFormatException ne)
+            {
+                JOptionPane.showMessageDialog(null, "Bitte eine natürliche Zahl eingeben");
+            }
+            
+        }
+        if(e.getActionCommand().equals("-"))
+        {
+            // Verringere den Wert um eins und nutze dazu eine passende Methode im model.
+            ?
+        }
+    }
+
+
+}
---
+++ VIEW.java
@@ -0,0 +1,101 @@
+/*
+ * Die hier importieren Pakete sind wichtig, um die grafische Darstellung
+ * zu ermöglichen. Das event-Paket wird benötigt, damit man Methoden schreiben
+ * kann, mit denen sich der Controller an den Buttons "anmelden" kann.
+ * 
+ */
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/*
+ * Die Klasse VIEW erbt von JFrame (einer Art Bilderrahmen, um Inhalte anzeigen
+ * zu können) und implementiert das Interface OBSERVER, um vom Model benachrichtigt
+ * werden zu können, wenn ein Update ansteht. Mehr dazu im Skript.
+ */
+public class VIEW extends JFrame implements OBSERVER
+{
+    private int zahl = 0;
+    private MODEL ?;
+    private JLabel label;
+    private ? buttonPlus;
+    private ? buttonMinus;
+    
+    /*
+     * Wenn eine View erstellt wird, muss ein zugehöriges Model angegeben werden. Das 
+     * erledigt bei uns der Controller.
+     * Das Panel, das erstellt wird, kann man sich vereinfacht als eine Art Brett mit
+     * Nägeln dran vorstellen, das in den Bilderrahmen (das JFrame bzw. hier die View
+     * selbst) gehängt wird, um daran dann die konkreten Elemente zu hängen.
+     * Diese Elemente sind ein Label (eine Art "Anzeige", auf der im Beispiel die Zahl 
+     * zu sehen ist) und zwei Buttons, ein Plus- und ein Minus-Button.
+     */
+    public VIEW(MODEL m)
+    {
+        model = m;
+        
+        JPanel panel = new JPanel();
+        
+        //Dem Panel wird ein bestimmtes Layout gegeben. Um mehr über Layouts zu erfahren,
+        //empfehle ich ggf eine Internetrecherche oder eine Nachfrage im Unterricht.
+        ?.setLayout(new FlowLayout());
+        //Das Label wird erstellt und soll die Zahl anzeigen.
+        label = new JLabel(""+ ?);
+        //Die Schriftart des Labels wird festgelegt.
+        ?.setFont(new Font("Serif", Font.PLAIN, 50));
+        //Die Position des Texts innerhalb des Labels wird festgelegt.
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        //Das Label wird zum Panel hinzugefügt.
+        ?.add(label);
+        
+        //Die Buttons werden erstellt und dem Panel hinzugefügt.
+        buttonPlus = new ?("+");
+        //Der Button wird dem panel hinzugefügt
+        panel.add(?);
+        
+        buttonMinus = new JButton(?);
+        //Der Button wird dem panel hinzugefügt
+        ?.add(?);
+        
+        //Das Panel wird in den Bilderrahmen/JFrame gehängt. Davor wird es nicht angezeigt !
+        this.add(panel);
+        
+        //Die Größe des JFrames wird festgelegt (Breite, Höhe in Pixeln).
+        // Verwende die Methode this.setSize(Breite, höhe)
+        ?(500, 150);
+        
+        //Das JFrane soll für den Nutzer sichtbar sein.
+        //Verwende die Methodethis.setVisible(true oder false);
+        ?
+        
+    }
+    
+    /*
+     * In der Methode update wird festgelegt, was passieren soll, wen ein Update
+     * ansteht. In diesem Fall soll der aktuelle Wert des Zählers vom Model geholt
+     * werden, in ein int verwandelt werden (er wird im Beispiel allgemein als "Object" 
+     * gesendet und muss erst wieder in das passende Datenformat gebracht werden).
+     * Anschließend wird der Inhalt des Labels neu gesetzt (und dadurch aktualisiert).
+     */
+    public ? update()
+    {
+        
+    }
+    
+    /*
+     * Die letzten beiden Methoden dienen dazu, dass sich ein ActionListener 
+     * (in unserem Fall der Controller) an einem Button "anmelden" kann, um dann
+     * informiert zu werden, wenn er gedrückt wird.
+     */
+    public void addPlusListener(ActionListener listenForPlusButton)
+    {
+        buttonPlus.addActionListener(listenForPlusButton);
+    }
+    
+    public void addMinusListener(ActionListener listenForMinusButton)
+    {
+        buttonMinus.addActionListener(listenForMinusButton);
+    }
+    
+}
